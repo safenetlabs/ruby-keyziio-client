bd8aeacc8332fe0cb9d8225d37e7201980174afa
hsharma001 <hsharma@safenet-inc.com>
2014-08-19 10:34:37 -0400

    Moved login stuff out to the test

--- lib/keyziio_client.rb
+++ lib/keyziio_client.rb
@@ -2,34 +2,9 @@ require 'keyziio_client/version.rb'
 require 'keyziio_client/kzrestclient.rb'
 require 'keyziio_client/kzcrypt.rb'
 
-class UnAuthenticatedUser < Exception
-  # User not logged in
-  def to_s
-    return 'User is not logged in.'
-  end
-end
-
 class Keyziio
-  attr_accessor :username
   def initialize
-    @logged_in = false
-    @username = nil
-    @asp_rest_client = KZRestClient.new()
-    @asp_rest_client.server_port = 3000
-    @asp_rest_client.server_url = 'localhost'
     @kzcrypt = KZCrypt.new()
-  end
-
-  def login_user (username)
-    # 'login: Logs the user in and sets up the users keyziio session.  Users are created automatically.
-    raise ArgumentError, 'Invalid arguments.  Expecting <username>' if username == nil
-    if @logged_in
-      return
-    end
-    response = @asp_rest_client.get("user_keys/#{username}")
-    @kzcrypt.inject_user_key(JSON.parse(response)['private_key'], JSON.parse(response)['id'])
-    @username = username
-    @logged_in = true
   end
 
   def inject_user_key (user_private_key_pem, key_id)
@@ -39,25 +14,14 @@ class Keyziio
 
   def encrypt_file (in_file, out_file, key_id)
     #'encrypt: Encrypts input_file with key_id to output_file.'
-    raise UnAuthenticatedUser if not @logged_in
     @kzcrypt.encrypt_file(in_file, out_file, key_id)
   end
 
   def decrypt_file (in_file, out_file)
     # 'decrypt: Decrypts input file to output file.  Gets the key_id from the file header.'
-    raise UnAuthenticatedUser if not @logged_in
+
     @kzcrypt.decrypt_file(in_file, out_file)
   end
 
-  def is_user_logged_in
-    return @logged_in
-  end
 end
 
-#if __FILE__ == $0
-#  # For debugging
-#  shell = Keyziio.new()
-#  shell.login_user('billy')
-#  shell.encrypt_file('README.md', 'readme_rb.enc', 'ruby_test_key1')
-#  #shell.decrypt_file('readme_rb.enc', 'readme.dec')
-#end
--- sample/keyziio_test.rb
+++ sample/keyziio_test.rb
@@ -1,27 +1,49 @@
 # A sample client that makes use of the keyziio library
 require 'keyziio_client'
 
+class UnAuthenticatedUser < Exception
+  # User not logged in
+  def to_s
+    return 'User is not logged in.'
+  end
+end
+
 class KzTest
   def initialize
-      @keyziio = Keyziio.new()
+    @logged_in = false
+    @username = nil
+    @asp_rest_client = KZRestClient.new()
+    @asp_rest_client.server_port = 3000
+    @asp_rest_client.server_url = 'localhost'
+    @keyziio = Keyziio.new()
   end
 
   def do_login (username)
     # 'login: Logs the user in and sets up the users keyziio session.  Users are created automatically.  Usage: login <username>'
-    if @keyziio.is_user_logged_in
-      print "Already logged in as #{@keyziio.username}\n"
+    if is_user_logged_in
+      print "Already logged in as #{@username}\n"
       return
     end
 
     print "logging #{username} in...\n"
-    @keyziio.login_user(username)
+    response = @asp_rest_client.get("user_keys/#{username}")
+
+    @keyziio.inject_user_key(JSON.parse(response)['private_key'], JSON.parse(response)['id'])
+    @username = username
+    @logged_in = true
+
     print "Successfully retrieved user key and ignited keyziio client\n"
     print "done\n"
   end
-    
+
+  def is_user_logged_in
+    return @logged_in
+  end
+
   def do_encrypt (*args)
     #'encrypt: Encrypts input_file with key_id to output_file: Usage: encrypt <input_file> <output_file> <key_id>'
     raise ArgumentError, 'Invalid arguments.  Usage: encrypt <input_file> <output_file> <key_id>' if args.length != 3
+    raise UnAuthenticatedUser if not @logged_in
     in_file, out_file, key_id = *args
     print "encrypting #{in_file} with key:#{key_id}...\n"
     @keyziio.encrypt_file(in_file, out_file, key_id)
@@ -31,6 +53,7 @@ class KzTest
   def do_decrypt (*args)
     # 'decrypt: Decrypts input file to output file.  Gets the key_id from the file header.  Usage: decrypt <input_file> <output_file>'
     raise ArgumentError, 'Invalid arguments.  Usage: decrypt <input_file> <output_file>' if args.length != 2
+    raise UnAuthenticatedUser if not @logged_in
     in_file, out_file = *args
     print "decrypting #{in_file}...\n"
     @keyziio.decrypt_file(in_file, out_file)
@@ -42,5 +65,5 @@ if __FILE__ == $0
   shell = KzTest.new()
   shell.do_login('billy')
   shell.do_encrypt('README.md', 'readme.enc', 'ruby_test_key1')
-  #shell.do_decrypt('readme.enc', 'readme.dec')
+  shell.do_decrypt('readme.enc', 'readme.dec')
 end
